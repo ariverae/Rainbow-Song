731e22a326b10216d28f4766e8f6f8f2d6f3508c
ariverae <ali.rivera@thalamuscorp.com>
2018-02-01 16:03:19 -0300

    conferencias jquery

--- js/main.js
+++ js/main.js
@@ -177,3 +177,21 @@ function initMap() {
 
   });
 })();
+
+$(function(){
+
+  // Programa de Conferencias
+  $('.programa-evento .info-curso:first').show();
+  $('.menu-programa a:first').addClass('activo');
+
+  $('.menu-programa a').on('click', function(){
+    $('.menu-programa a').removeClass('activo')
+    $(this).addClass('activo');
+    $('.ocultar').hide();
+    var enlace = $(this).attr('href');
+    $(enlace).fadeIn(1000);
+
+      return false;
+  });
+
+});
